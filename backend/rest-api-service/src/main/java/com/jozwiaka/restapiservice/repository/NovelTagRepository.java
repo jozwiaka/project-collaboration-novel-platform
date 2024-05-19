130f21adcc9e98c9cf2e4ee7873d1a325d0b6ce5
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-05-19 17:09:07 +0200

    Update

--- backend/rest-api-service/src/main/java/com/jozwiaka/restapiservice/repository/NovelTagRepository.java
+++ backend/rest-api-service/src/main/java/com/jozwiaka/restapiservice/repository/NovelTagRepository.java
@@ -2,11 +2,13 @@ package com.jozwiaka.restapiservice.repository;
 
 import com.jozwiaka.restapiservice.model.NovelTag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
 
-//@CrossOrigin("http://localhost:4200")
-//@RepositoryRestResource(collectionResourceRel = "novelTags", path = "novel-tags")
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "novelTags", path = "novel-tags")
 public interface NovelTagRepository extends JpaRepository<NovelTag, Integer> {
     Optional<NovelTag> findByNovelIdAndTagId(Integer novelId, Integer tagId);
 }
